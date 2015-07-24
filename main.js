afe8568052f28f3d520f787ce36168fbcee68403
Andrew Lopez <Andy@Andrews-MacBook-Pro.local>
2015-07-24 15:17:59 -0700

    rebuild

--- main.js
+++ main.js
@@ -11,15 +11,16 @@ var displayContent = "";
 var numArray = ["", ""];
 var arrayIndex = 0;
 /*********
-*Number pads input switch that will choose what number to input and concat it to the
-*current arrayIndex's string
-*
-*********/
+ *Number pads input switch that will choose what number to input and concat it to the
+ *current arrayIndex's string
+ *
+ *********/
 $(document).ready(function() {
     //switch to determine input into array
     $(".number").click(function() {
         console.log('Current position in number array is: ' + arrayIndex);
         operatorSelect = false;
+        //displayContent gets assigned new value with each click on a number
         var inputvar = $("#result_display").val();
         switch (true) {
             case $(this).hasClass('n0'):
@@ -62,11 +63,11 @@ $(document).ready(function() {
         $("#result_display").val(displayContent);
         console.log(displayContent);
     });
-/*******
-*on click handler for any of the operator buttons that will set the 
-*operator variable (dividing/multiplying/etc...) for the evaluate function's
-*switch.
-*******/
+    /*******
+     *on click handler for any of the operator buttons that will set the 
+     *operator variable (dividing/multiplying/etc...) for the evaluate function's
+     *switch.
+     *******/
     $(".operator").click(function() {
         if (!operatorSelect) {
             switch (true) {
@@ -134,28 +135,28 @@ $(document).ready(function() {
             console.log('arrayIndex changed to 1')
         }
     });
-// supposed to be inserting a '-' infront of the current arrayIndex's value
-    $(".plusmin").click(function() {
-        if (!negative) {
-            var negStore = "-" + $("#result_display").val();
-            $("#result_display").val(negStore);
-            negative = true;
-        } else {
-            var storeValue = $("#result_display").val();
-            $("#result_display").val(storeValue.slice(1));
-            negative = false;
-        }
-    });
-
-    /*Comment*
+    // supposed to be inserting a '-' infront of the current arrayIndex's value
+    //DWP THIS ONE
+    //Calls this function when the .plusmin class html element is clicked
+    //checks if the negative var is true then if the value in the current array slot is positive executes this block
+    //concats a - into the front of the current array slot's value
+    //sets the negative variable for any future calls to this function
+    //if the first 'if' function was false this else statement slices out the first value in current array slot
+    /********
      *Equals <span> on click event will call evaluate function
      *function evaluate() takes value in numarray 0 and 1 and does math
      *then prints out to display and clears the input array
-     *End Comment*/
+     *******/
     function evaluate() {
         numArray[arrayIndex] = $("#result_display").val();
         var results = "";
         switch (true) {
+            /*
+             *check operator variable for a true boolean value;
+             *sets the input field with ID 'result_display' to the selected operator
+             *grabs the strings in the numArray converting it to a number then does the math to them. and sets the result variable to the answer
+             *breaks out of the switch function once an operation has completed
+             */
             case dividing:
                 $("#result_display").val("/");
                 results = Number(numArray[0]) / Number(numArray[1]);
@@ -174,13 +175,13 @@ $(document).ready(function() {
                 break;
             default:
                 console.log('ERROR');
-        }
+        } ///Checks if the result is Infinity to see if divide by 0 was entered
         if (results == Infinity) {
             results = "Error: dividing by 0";
         }
-        $("#result_display").val(results);
+        $("#result_display").val(results); //grabs the value from the result variable and puts it into the result_display
         console.log($("#result_display").val());
-        arrayIndex = 0;
+        arrayIndex = 0; //Resets the position of where input from buttons goes to back to the first entry in the numArray
     }
     //calls the evaluate function with a click event handler
     $(".evaluate").click(function() {
